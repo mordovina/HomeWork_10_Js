4369522a9aadea5a2d270e5f8a8b93ca7eead5d8
yuliya mordovina <mordovina_y@list.ru>
2023-03-17 17:16:09 +0300

    homework 10

---
+++ data.js
@@ -0,0 +1,26 @@
+const productData = [
+    {
+    src:'img.png',
+        alt:'img obj data',
+        name:'Название из obj data',
+        link:'product.html',
+        desс: 'Описание товара из obj data',
+        price: 21000
+    },
+    {
+    src:'img2.png',
+        alt:'img obj data new photo',
+        name:'Название нового товара из obj data',
+        link:'product2.html',
+        desс: 'Описание нового товара из obj data',
+        price: 11000
+    },
+    {
+    src:'img3.png',
+        alt:'img obj data new photo 3',
+        name:'Название нового товара из obj data 3',
+        link:'product3.html',
+        desс: 'Описание нового товара из obj data 3',
+        price: 31000
+    }
+]
